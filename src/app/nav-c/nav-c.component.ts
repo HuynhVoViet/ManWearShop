38ec55f157fb74ce55e317212c0352519d3761b1
HuynhVoViet <vohuynhvietpro11@gmail.com>
2021-03-07 21:30:02 +0700

    DropDownAvatar

--- src/app/nav-c/nav-c.component.ts
+++ src/app/nav-c/nav-c.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faBars, faSearch, faBell, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faSearch, faBell, faEnvelope, faUser, faCog, faPowerOff } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-nav-c',
   templateUrl: './nav-c.component.html',
@@ -10,6 +10,14 @@ export class NavCComponent implements OnInit {
   faSearch = faSearch;
   faBell = faBell;
   faEnvelope = faEnvelope;
+  faUser = faUser;
+  faCog = faCog;
+  faPowerOff = faPowerOff;
+  dropdownContent = true;
+
+  onButtonClick(){
+    this.dropdownContent = !this.dropdownContent;
+  }
   constructor() { }
 
   ngOnInit(): void {
